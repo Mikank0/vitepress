d9184149c5b6696c51cd79607d658b3e466842c4
Yihan Wu <82814254+Mikank0@users.noreply.github.com>
2025-11-23 16:53:30 +0800

    Update sidebar.ts

--- docs/.vitepress/configs/sidebar.ts
+++ docs/.vitepress/configs/sidebar.ts
@@ -1,3 +1,16 @@
 import type { DefaultTheme } from 'vitepress'
+import { slugify } from '@mdit-vue/shared'
+// 注意路径：从 configs 目录往上跳两级才能找到 docs/nav
+import { NAV_DATA } from '../../nav/data'
 
-export const sidebar: DefaultTheme.Config['sidebar'] = {}
+export const sidebar: DefaultTheme.Config['sidebar'] = {
+  '/nav/': [
+    {
+      text: '导航目录', // 这里是侧边栏顶部的标题
+      items: NAV_DATA.map((category) => ({
+        text: category.title,
+        link: `/nav/#${slugify(category.title)}`
+      }))
+    }
+  ]
+}
