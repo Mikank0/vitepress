4374d309c55f638086f72c38c68e56f18f93d176
Yihan Wu <82814254+Mikank0@users.noreply.github.com>
2025-11-23 22:21:57 +0800

    Update nav.ts

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -2,14 +2,9 @@ import type { DefaultTheme } from 'vitepress'
 
 export const nav: DefaultTheme.Config['nav'] = [
   { text: '网页导航', link: '/nav/' },
-  { text: '个人主页', link: 'https://fe-mm.com' },
+  { text: '个人主页', link: 'https://ieeexplore.ieee.org/author/37086226053' },
   {
-    text: '茂茂物语',
-    link: 'https://notes.fe-mm.com',
-  },
-  { text: 'mmPlayer', link: 'https://netease-music.fe-mm.com' },
-  {
-    text: '油猴脚本',
-    link: 'https://github.com/maomao1996/tampermonkey-scripts',
+    text: '实验室官网',
+    link: 'https://pearl.shanghaitech.edu.cn/',
   },
 ]
